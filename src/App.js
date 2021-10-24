1683267549267ac3290891cc72ddaa1cb2c4df47
Rodrigo <rodasaraujo97@gmail.com>
2021-10-24 17:30:30 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import ContactCard from './components/ContactCard';
 
 function App() {
-  return (
-    <div className="App">
+  const [contacts, setContacts] = useState([]);
 
+  useEffect(() => {
+    try {
+      axios.get(`https://randomuser.me/api`).then((res) => {
+        console.log(res.data.results);
+        setContacts(res.data.results);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  return (
+    <div>
+      {contacts.length > 0 &&
+        contacts.map((contacts, index) => {
+          return (
+            <ContactCard
+              key={index}
+              avatarUrl={contacts.picture.large }
+              name={contacts.name.first}
+              email={contacts.email}
+              age={contacts.dob.age}
+            />
+          );
+        })}
     </div>
   );
 }
